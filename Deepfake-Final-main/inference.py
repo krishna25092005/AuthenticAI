8fde1817f74a0d86feb10c03cf565158a516b7a1
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2025-02-10 07:22:34 +0530

    Update inference.py

--- Deepfake-Final-main/inference.py
+++ Deepfake-Final-main/inference.py
@@ -7,6 +7,10 @@ import numpy as np
 model = load_model('/mount/src/authenticai/Deepfake-Final-main/final-model-1.h5')  # Adjust path if needed
 
 def preprocess_image(image):
+    # Convert RGBA (4 channels) or Grayscale (1 channel) to RGB (3 channels)
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    
     img = image.resize((224, 224))  # Resize image for model input
     img_array = np.array(img) / 255.0  # Normalize
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
